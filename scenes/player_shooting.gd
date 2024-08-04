8d85ae30e3fdd48deb018205cdf898f2ad6439b5
Springcomp <springcomp@users.noreply.github.com>
2024-08-04 15:13:57 +0200

    [ship] shooting

---
+++ scenes/player_shooting.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var bullet_scene: PackedScene
+
+func _process(_delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var bullet = bullet_scene.instantiate() as Bullet
+	bullet.global_position = global_position
+	bullet.direction = Vector2(0, 1).rotated(get_parent().rotation)
+	get_tree().root.add_child.call(bullet);
