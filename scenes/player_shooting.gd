2198842ead8a19043837758a1a4d5a75740919c1
Springcomp <springcomp@users.noreply.github.com>
2024-08-13 13:58:53 +0200

    simplified dependencies

--- scenes/player_shooting.gd
+++ scenes/player_shooting.gd
@@ -2,8 +2,6 @@ extends Node2D
 
 @export var bullet_scene: PackedScene
 
-@onready var player: Player = $".."
-
 func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -11,6 +9,6 @@ func _process(_delta):
 func shoot():
 	var bullet = bullet_scene.instantiate() as Bullet
 	bullet.global_position = global_position
-	bullet.direction = Vector2(0, 1).rotated(player.rotation)
+	bullet.direction = Vector2(0, 1).rotated(get_parent().rotation)
 
 	get_tree().root.add_child.call_deferred(bullet)
