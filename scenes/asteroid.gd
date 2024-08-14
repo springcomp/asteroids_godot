4766b8f650a4f4763f9072685cef739377cae4fd
Springcomp <springcomp@users.noreply.github.com>
2024-08-14 23:53:17 +0200

    [asteroids] refactors and simplifies explosion

--- scenes/asteroid.gd
+++ scenes/asteroid.gd
@@ -9,8 +9,8 @@ signal on_destroyed(position: Vector2, size: Utils.AsteroidSize)
 @export var asteroid_scene: PackedScene
 @export var speed: float = 100
 
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var explosion_particles: ExplosionParticles = $ExplosionParticles
+@onready var sprite = $Sprite2D
+@onready var explosion_particles: CPUParticles2D = $ExplosionParticles
 
 const rotation_speeds = [
 	PI / 20,
@@ -28,6 +28,7 @@ const asteroid_textures = [
 var direction: Vector2
 var size: Utils.AsteroidSize
 var rotation_speed: float = PI / 20
+var exploding: bool = false
 
 func _ready():
 	direction = Vector2(0, 1).rotated(rotation)
@@ -46,20 +47,38 @@ func _process(delta: float):
 	rotation += rotation_speed * delta
 
 func _on_area_entered(area: Area2D):
+	if exploding:
+		return
+		
 	if area is Bullet:
 		area.queue_free()
 		on_destroy()
 
 func _on_body_entered(body: CharacterBody2D):
+	if exploding:
+		return
+
 	if body is Player && !(body as Player).invincible:
 		body.destroy()
 		on_destroy()
 
 func on_destroy():
+	exploding = true
 	explode()
 	on_destroyed.emit(position, size)
-	queue_free()
 
 func explode():
-	explosion_particles.reparent(get_tree().root)
-	explosion_particles.explode()
+	sprite.visible = false
+	
+	## works around a bug that
+	## prevents finished signal
+	## from being fired
+	
+	explosion_particles.emitting = false
+	explosion_particles.one_shot = true
+	explosion_particles.emitting = true
+	
+	explosion_particles.finished.connect(on_explosion_particles_finished)
+	
+func on_explosion_particles_finished():
+	queue_free()
